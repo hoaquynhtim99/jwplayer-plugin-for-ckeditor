098dde0b77abf4a321c07770f536797ae1329469
Phan Tan Dung <phantandung92@gmail.com>
2019-11-13 11:37:37 +0700

    Cập nhật tương thích NukeViet 4.3

--- includes/plugin/ckeditor_jwplayer.php
+++ includes/plugin/ckeditor_jwplayer.php
@@ -14,5 +14,5 @@ if (! defined('NV_MAINFILE')) {
 global $my_footer;
 
 $my_footer .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . NV_ASSETS_DIR . "/images/jwplayer/jwplayer.js\"></script>\n";
-$my_footer .= "<script type=\"text/javascript\">jwplayer.key=\"KzcW0VrDegOG/Vl8Wb9X3JLUql+72MdP1coaag==\";</script>\n";
+$my_footer .= "<script type=\"text/javascript\">jwplayer.key=\"YgtWotBOi+JsQi+stgRlQ3SK21W2vbKi/K2V86kVbwU=\";</script>\n";
 $my_footer .= "<script type=\"text/javascript\" src=\"" . NV_BASE_SITEURL . NV_ASSETS_DIR . "/images/jwplayer/jwplayer.trigger.js\"></script>\n";
